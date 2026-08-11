40a88e6489662fc10217f722322651c19ad79f84
praisegod-code <brightpraisegod966@gmail.com>
2026-08-11 11:54:39 +0100

    build accounts authentication foundation

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import PassengerRegistrationForm
+
+
+def register(request):
+    if request.method == "POST":
+        form = PassengerRegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+
+    else:
+        form = PassengerRegistrationForm()
+
+    return render(request, "accounts/register.html", {"form": form})
