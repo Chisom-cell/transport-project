3344891c4638dc4333c89a4a333f60e703d2d304
chibor554-code <chibor554@gmail.com>
2026-08-13 12:04:52 +0100

    commit my notification

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("transport/", include("transport.urls")),
+    path("notifications/", include("notifications.urls")),
 ]
